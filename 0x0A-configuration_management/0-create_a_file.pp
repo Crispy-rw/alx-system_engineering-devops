f4895e575f45c186d0b3eec1fc38ffdff00efda5
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-15 08:00:59 +0000

    ft: feat create

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creates a file in the tmp dir of the node
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
